956e56efafc867ba0aafa6f87592395fb380e740
watashiwalds <dangson2124.ld@gmail.com>
2023-10-14 12:50:47 +0700

    minor change to <html> box-sizing

--- appleWatch/offers/ultra2.js
+++ appleWatch/offers/ultra2.js
@@ -65,13 +65,13 @@ function set_bandcolor(type) {
     bandcolor = type;
     var get_displaybandcolor = document.getElementById("dt_bandcolor");
     if (type === "al_1") get_displaybandcolor.innerHTML = "Blue"; else
-    if (type === "tr_1") get_displaybandcolor.innerHTML = "Blue/Black"; else
-    if (type === "oc_1") get_displaybandcolor.innerHTML = "Blue"; else
     if (type === "al_2") get_displaybandcolor.innerHTML = "Indigo"; else
-    if (type === "tr_2") get_displaybandcolor.innerHTML = "Green/Gray"; else
-    if (type === "oc_2") get_displaybandcolor.innerHTML = "Orange"; else
     if (type === "al_3") get_displaybandcolor.innerHTML = "Olive"; else
+    if (type === "tr_1") get_displaybandcolor.innerHTML = "Green/Gray"; else
+    if (type === "tr_2") get_displaybandcolor.innerHTML = "Blue/Black"; else
     if (type === "tr_3") get_displaybandcolor.innerHTML = "Orange/Beige"; else
+    if (type === "oc_1") get_displaybandcolor.innerHTML = "Blue"; else
+    if (type === "oc_2") get_displaybandcolor.innerHTML = "Orange"; else
     if (type === "oc_3") get_displaybandcolor.innerHTML = "White";
 }
 function clear_bandcolor() {
@@ -87,13 +87,74 @@ function set_bandsize(type) {
 }
 
 function show_detail() {
-    alert("detail");
+    document.getElementById("moreinfo").style.display = "block";
+}
+function hide_detail() {
+    document.getElementById("moreinfo").style.display = "none";
 }
 
-function beginorder() {
+var get_purchaseform = document.getElementById("purchase");
+var get_buyquantity = document.getElementById("buyquantity");
+let buy_quantity;
+const base_price = 21990000;
+function begin_order() {
     if (bandcolor!=undefined && bandtype!=undefined && bandsize!=undefined) {
-        alert("Sẵn sàng để thực hiện mua hàng. Chức năng đang phát triển");
+        get_purchaseform.style.display = "block";
+        buy_quantity = 1;
+        document.getElementById("pc_bandtype").innerHTML = bandtype==="al"?"Alpine Loop":bandtype==="tr"?"Trail Loop":"Ocean Band";
+        document.getElementById("pc_bandsize").innerHTML = bandsize==="sm"?"Nhỏ - 130-160mm":bandsize==="md"?"Vừa - 145-190mm":"Lớn - 165-210mm";
+        document.getElementById("pc_totalprice").innerHTML = base_price.toLocaleString("en-US") + "đ";
+        var get_pcbandcolor = document.getElementById("pc_bandcolor");
+        var get_pc_imgpv = document.getElementById("pc_imgpv");
+        if (bandcolor === "al_1") {
+            get_pcbandcolor.innerHTML = "Blue";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_alpine_blue_side.png");
+        } else
+        if (bandcolor === "al_2") {
+            get_pcbandcolor.innerHTML = "Indigo";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_alpine_indigo_side.png");
+        } else
+        if (bandcolor === "al_3") {
+            get_pcbandcolor.innerHTML = "Olive";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_alpine_olive_side.png");
+        } else
+        if (bandcolor === "tr_1") {
+            get_pcbandcolor.innerHTML = "Green/Gray";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_trail_greengray_side.png");
+        } else
+        if (bandcolor === "tr_2") {
+            get_pcbandcolor.innerHTML = "Blue/Black";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_trail_blueblack_side.png");
+        } else
+        if (bandcolor === "tr_3") {
+            get_pcbandcolor.innerHTML = "Orange/Beige";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_trail_orangebeige_side.png");
+        } else
+        if (bandcolor === "oc_1") {
+            get_pcbandcolor.innerHTML = "Blue";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_ocean_blue_side.png");
+        } else
+        if (bandcolor === "oc_2") {
+            get_pcbandcolor.innerHTML = "Orange";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_ocean_orange_side.png");
+        } else
+        if (bandcolor === "oc_3") {
+            get_pcbandcolor.innerHTML = "White";
+            get_pc_imgpv.setAttribute("src", "./img_ultra2/ultra2_ocean_white_side.png");
+        }
     } else {
         alert("Quý khách hàng hãy chọn đầy đủ thông số trước khi mua hàng!");
+    }
+}
+function cancel_order() {
+    get_purchaseform.style.display = "none";
+    get_buyquantity.value = 1;
+}
+function update_totalprice() {
+    if (get_buyquantity.value>0 && get_buyquantity.value<11) { 
+        buy_quantity = get_buyquantity.value;
+        document.getElementById("pc_totalprice").innerHTML = (base_price*buy_quantity).toLocaleString("en-US") + "đ";
+    } else {
+        get_buyquantity.value = buy_quantity;
     }
 }
