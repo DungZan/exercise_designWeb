ec1567cc7d3089f020ea79eac99e8fc687de8608
watashiwalds <dangson2124.ld@gmail.com>
2023-10-14 13:12:36 +0700

    near-finish buying page

--- appleWatch/offers/ultra2.js
+++ appleWatch/offers/ultra2.js
@@ -158,3 +158,18 @@ function update_totalprice() {
         get_buyquantity.value = buy_quantity;
     }
 }
+let fin_gender, fin_name, fin_phone, fin_address, fin_payment;
+function confirm_payment() {
+    if (document.getElementById("gender-male").checked) fin_gender = 0; else
+    if (document.getElementById("gender-female").checked) fin_gender = 1;
+    fin_name = document.getElementById("fullname").value;
+    fin_phone = document.getElementById("phone").value;
+    fin_address = document.getElementById("address").value;
+    if (document.getElementById("payment-cash").checked) fin_payment = 0; else
+    if (document.getElementById("payment-bank").checked) fin_payment = 1;
+    if (fin_gender == undefined || fin_name == undefined || fin_phone == undefined || fin_address == undefined || fin_payment == undefined || fin_name == "" || fin_phone == "" || fin_address == "") {
+        alert("Quý khách hàng hãy nhập đầy đủ thông tin trước khi thanh toán!");
+    } else {
+        alert("Đơn hàng của quý khách đã được ghi nhận!");
+    }
+}
