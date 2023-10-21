ae6adb1cd1c5be4a2863832c6d646308ccaab048
Alex <tmquang031104@gmail.com>
2023-10-21 22:20:24 +0700

    sửa phần infor

--- total.js
+++ total.js
@@ -14,7 +14,7 @@ buttons.forEach(function(button){
 });
 
 var check_phone = /^\(\d{2,4}\)[\s\.-]\d{3}[\s\.-]\d{3}$/;
-var check_email = /^([\w-]+(\?\:\/[\w]+)*)@((\?\:[\w-]+\.)*\w[w-]{0,6})\.([a-z]{2,6}(\?\:\.[a-z]{2})?)$/;
+var check_email = /^([\w-]+(\?\:\.[\w-]+)*)@((\?\:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(\?\:\.[a-z]{2})?)$/;
 var check_username = /^[A-Za-z0-9_]{1,20}$/;
 var check_password = /^[A-Za-z0-9!@#$%^&*()_]{6,20}$/;
 
@@ -24,7 +24,7 @@ function checknull(txt){
 }
 
 function isInteger(txt){
-    if((!isNaN(txt.value)) && (parrseInt(txt.value) === Number(txt.value)))
+    if((!isNaN(txt.value)) && (parseInt(txt.value) === Number(txt.value)))
         return true;
     else
         return false;
@@ -32,11 +32,11 @@ function isInteger(txt){
 
 function notCheck(radio){
     var rt = true;
-    for(i = 0; i < radio.length; i++){
+    for(i = 0; i < radio.length; i++)
         if(radio[i].checked)
             rt = false;
-    }
     return rt;
+    
 }
 
 function StringMatch(txt,reg){
@@ -45,29 +45,29 @@ function StringMatch(txt,reg){
 
 function validform(f){
     if(checknull(f.fullname)){
-        alert(f.fullname.name + "must be not null");
+        alert(f.fullname.name + " must be not null");
         f.fullname.focus();
         return;
     }
     if(checknull(f.age)){
-        alert(f.age.name + "must be not null");
+        alert(f.age.name + " must be not null");
         f.age.focus();
         return;
     }
     if(isNaN(f.age.value)){
-        alert(f.age.name + "must be a number");
+        alert(f.age.name + " must be a number");
         f.age.value = "";
         f.age.focus();
         return;
     }
     if(!isInteger(f.age)){
-        alert(f.age.name + "must be an integer number");
+        alert(f.age.name + " must be an integer number");
         f.age.value = "";
         f.age.focus();
         return;
     }
     if(eval(f.age.value) <= 0 || eval(f.age.value) >= 113){
-        alert(f.age.name + "must be in (0 - 113)");
+        alert(f.age.name + " must be in (0 - 113)");
         f.age.value = "";
         f.age.focus();
         return;
@@ -78,11 +78,11 @@ function validform(f){
     }
     if(!StringMatch(f.phone, check_phone)){
         alert("Phone is not valid");
-        f.phone.validform = "";
+        f.phone.value = "";
         f.phone.focus();
         return;
     }
-    if(!StringMatch(f.email,check_email)){
+    if(!StringMatch(f.email, check_email)){
         alert("Email is not valid");
         f.email.value = "";
         f.email.focus();
